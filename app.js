72b9fe99f234befc0662b453920ab521c0aed8dc
Cody Young <cyoung1623@gmail.com>
2019-07-21 19:12:15 -0700

    Initial website layout

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("index.html");
+});
+
+//Server listener
+app.listen("8081", "127.0.0.1", function() {
+    console.log("Express Server is running...");
+});
+
+app.get("/mercury", function(req, res) {
+    res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res) {
+    res.render("venus.html");
+});
