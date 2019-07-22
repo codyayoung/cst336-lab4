3b2c5de1a3cb5489b7ebd833edf709e9c383e99c
Cody Young <cyoung1623@gmail.com>
2019-07-21 19:18:24 -0700

    Heroku deployment patch

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.get("/", function(req, res) {
 });
 
 //Server listener
-app.listen("8081", "127.0.0.1", function() {
-    console.log("Express Server is running...");
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Running Express server...");
 });
 
 app.get("/mercury", function(req, res) {
